6b9d088cc0b91ec8f714b43734d325cd46e1d4e5
michaelmensa <michaelmensa@live.com>
2024-02-08 21:40:21 +0000

    task0: creating redis client

---
+++ utils/redis.js
@@ -0,0 +1,40 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient({
+      url: 'redis://127.0.0.1:6380'
+    });
+    this.client.on('error', err => console.log('Redis Client Error', err));
+  }
+
+  // returns true when connection to Redis is a success or false if otherwise
+  isAlive() {
+    return !this.client.connected;
+  }
+
+  // async get that takes key as arg and returns the value stored for this key
+  async get(key) {
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    return asyncGet(key);
+  }
+
+  // async set that takes key, value and duration as args to store in redis
+  // with an expiration set by duration arg
+  async set(key, value, duration) {
+    const asyncSet = promisify(this.client.set).bind(this.client);
+    setTimeout(() => {
+      asyncSet(key, value);
+    }, duration);
+  }
+
+  // async del that takes key as arg and remove the value for this key
+  async del(key) {
+    const asyncDel = promisify(this.client.del).bind(this.client);
+    asyncDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
