8b6c40d8d4a96c13bb18ad9c9059a70573b2accc
michaelmensa <michaelmensa@live.com>
2024-02-14 11:51:18 +0000

    task 2

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 // use body-parser to parse JSON data sent in request body
 
+// port
+const port = process.env.PORT || 5000;
+
 const app = express();
 
 // Middleware to parse JSON bodies
@@ -10,7 +13,7 @@ const parser = app.use(bodyParser.json());
 
 app.use('/', routes);
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log('Server running on port 5000');
 });
 
